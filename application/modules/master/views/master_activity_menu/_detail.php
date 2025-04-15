d7b15b1e1a2ae909b0b94e5f200c04bd27c11150
Rara <tiarasanir@gmail.com>
2025-04-15 11:51:02 +0700

    Add

---
+++ application/modules/master/views/master_activity_menu/_detail.php
@@ -0,0 +1,29 @@
+									<div class="row">
+										<label class="col-md-3 control-label no-padding-right">Activity Name</label>
+										<div class="col-md-9">
+											: <span class="activity"></span>
+										</div>
+									</div>
+									<div class="row">
+										<label class="col-md-3 control-label no-padding-right">Activity Parent</label>
+										<div class="col-md-9">
+											: <span class="activity_parent"></span>
+										</div>
+									</div>
+									<div class="row">
+										<label class="col-md-3 control-label no-padding-right">Job Order</label>
+										<div class="col-md-9">
+											: <span class="job_order"></span>
+										</div>
+									</div>
+									
+									<div class="row">
+										<label class="col-md-3 control-label no-padding-right">Is Active</label>
+										<div class="col-md-9">
+											: <span class="is_active"></span>
+										</div>
+									</div>
+									
+									
+
+									
---
+++ application/modules/master/views/master_activity_menu/_field.php
@@ -0,0 +1,25 @@
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Activity Name <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$txtactivity;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Activity Parent <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$selactivityparent;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Job Order <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$selorder;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Is Active <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$rdoisactive;?>
+										</div>
+									</div>
+									
