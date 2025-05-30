b7ff74cbcac1e29aa93f5d7665449406c4408a49
Rara <tiarasanir@gmail.com>
2025-05-30 11:24:52 +0700

    update

--- application/modules/dashboard/views/dashboard_detail_menu/view.php
+++ application/modules/dashboard/views/dashboard_detail_menu/view.php
@@ -81,7 +81,7 @@ if  (_USER_ACCESS_LEVEL_VIEW == "1" && _USER_ACCESS_LEVEL_EKSPORT == "1") {
 	<div class="col-md-12 col-sm-12">
 		<div class="portlet box green">
 			<div class="portlet-title">
-				<div class="caption"><span class="title_maps">CCTV Streaming..</span></div>
+				<div class="caption"><span class="title_maps">CCTV Streaming</span></div>
 				<div class="actions">
 					
 				</div>
